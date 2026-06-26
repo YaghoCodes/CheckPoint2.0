fccca1004166a18da5bb757404af18f780fa1821
YaghoCodes <yagho.chinaglia@sptech.school>
2026-06-26 19:56:15 -0300

    Adaptações de html e css feitas da home, login, cadastro, exclusão de arquivos desnecessarios, e ajustes nas rotas e controllers de login e cadastro ja funcionando 100%, alterações do bd e modelagem logica

--- web-data-viz/src/database/script-tabelas.sql
+++ web-data-viz/src/database/script-tabelas.sql
@@ -1,58 +1,37 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+drop database checkpoint;
+CREATE DATABASE if not exists checkpoint;
+USE checkpoint;
 
-/*
-comandos para mysql server
-*/
+drop table if exists usuario;
+drop table if exists jogo;
+drop table if exists avaliacao;
 
-CREATE DATABASE aquatech;
+CREATE TABLE usuario(
+  id_usuario INT NOT NULL AUTO_INCREMENT,
+  username VARCHAR(45) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id_usuario)
+) AUTO_INCREMENT = 1;
 
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+CREATE TABLE jogo(
+  id_jogo INT NOT NULL,
+  nome VARCHAR(100) NOT NULL,
+  categoria VARCHAR(20) NOT NULL,
+  description TEXT NOT NULL,
+  developer VARCHAR(100) NOT NULL,
+  imagem VARCHAR(255) null,
+  PRIMARY KEY (id_jogo)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE avaliacao (
+  fk_usuario INT NOT NULL,
+  fk_jogo INT NOT NULL,
+  status TINYINT NOT NULL DEFAULT 0, -- 0 = WishList 1 = Jogando 2 = Concluido 3 = nada --
+  review TEXT NULL,
+  nota INT NULL,
+  PRIMARY KEY (fk_usuario, fk_jogo),
+  CONSTRAINT fk_Avaliacoes_Usuario FOREIGN KEY (fk_usuario) REFERENCES usuario (id_usuario),
+  CONSTRAINT fk_Avaliacoes_Jogos FOREIGN KEY (fk_jogo) REFERENCES jogo (id_jogo),
+  CONSTRAINT chk_nota CHECK (nota BETWEEN 0 AND 5)
 );
-
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
-);
-
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
-);
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	temperatura DECIMAL,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
